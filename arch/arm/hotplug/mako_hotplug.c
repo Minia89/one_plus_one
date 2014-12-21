7b4ef9a5cfa1bfab1c7d2efa9a18e0e60818525d
Evisceration <eviscerationls@gmail.com>
2014-12-21 13:54:48 +0100

    mako_hotplug: prevent double suspend and resume
    Change-Id: Ie830d9eed9ea7a1d6fd5d536dc684b691d6059da

--- arch/arm/hotplug/mako_hotplug.c
+++ arch/arm/hotplug/mako_hotplug.c
@@ -312,6 +312,9 @@ static void mako_hotplug_suspend(struct work_struct *work)
 	if (!t->enabled)
 		return;
 
+	if (stats.suspend)
+		return;
+
 	/*
 	 * Save the current max freq before capping it to 1GHz
 	 * so that we can restore it after screen on.
@@ -350,6 +353,9 @@ static void __ref mako_hotplug_resume(struct work_struct *work)
 	int cpu;
 
 	if (!t->enabled)
+		return;
+
+	if (!stats.suspend)
 		return;
 
 	stats.screen_cap_lock = true;
