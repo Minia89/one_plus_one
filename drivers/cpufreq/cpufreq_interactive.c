1a0f05cb487e5e41ce90c29b37c50d423e4d394a
DerRomtester <dieb.stefan.96@gmail.com>
2015-01-19 15:07:50 +0100

    cpufreq: interactive: 90 seems to be a good highspeed value

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -73,7 +73,7 @@ static struct mutex gov_lock;
 static unsigned int hispeed_freq = DEFAULT_HISPEED_FREQ;
 
 /* Go to hi speed when CPU load at or above this value. */
-#define DEFAULT_GO_HISPEED_LOAD 99
+#define DEFAULT_GO_HISPEED_LOAD 90
 static unsigned long go_hispeed_load = DEFAULT_GO_HISPEED_LOAD;
 
 /* Target load.  Lower values result in higher CPU speeds. */
