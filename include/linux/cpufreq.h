86b1b3a4abbaf981eec124ad3b6e643604dc5a99
DerRomtester <dieb.stefan.96@gmail.com>
2015-02-06 17:44:06 +0100

    cpufreq: add yankactive and impulse https://github.com/neobuddy89/hammerhead/commit/5bcdd1e0e249550bafadaf201f9e4722abd0c533 https://github.com/neobuddy89/hammerhead/commit/b4a66f303bbd027fc4c8c5ecdc919bea989c9231

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -423,6 +423,12 @@ extern struct cpufreq_governor cpufreq_gov_optimax;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIMM)
 extern struct cpufreq_governor cpufreq_gov_intellimm;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_intellimm)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKACTIVE)
+extern struct cpufreq_governor cpufreq_gov_yankactive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_yankactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
+extern struct cpufreq_governor cpufreq_gov_impulse;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_impulse)
 #endif
 
 
