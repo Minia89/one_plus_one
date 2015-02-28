f226394350e35f5146d7548274a66bca328916b1
DerRomtester <dieb.stefan.96@gmail.com>
2015-02-28 16:53:22 +0100

    cpufreq: hyper: remove it because of stability issues

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -389,9 +389,6 @@ extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_UBERDEMAND)
 extern struct cpufreq_governor cpufreq_gov_uberdemand;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_uberdemand)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HYPER)
-extern struct cpufreq_governor cpufreq_gov_hyper;
-#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_hyper)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMAND)
 extern struct cpufreq_governor cpufreq_gov_intellidemand;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_intellidemand)
