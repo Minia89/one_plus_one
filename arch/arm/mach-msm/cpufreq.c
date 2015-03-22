08a36763848155304c7f2fe0593c1e8c517ad16f
Minia89 <n.miniati@gmail.com>
2015-03-22 02:08:18 +0100

    cpufreq: revert

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -41,13 +41,6 @@ static bool hotplug_ready;
 static struct cpufreq_frequency_table *krait_freq_table;
 #endif
 
-/* maxscroff */
-uint32_t maxscroff_freq = 1190400;
-uint32_t maxscroff = 0; 
-
-/* ex max freq */
-uint32_t ex_max_freq;
-
 struct cpufreq_suspend_t {
 	struct mutex suspend_mutex;
 	int device_suspended;
@@ -190,7 +183,6 @@ static int msm_cpufreq_init(struct cpufreq_policy *policy)
 		return ret;
 	pr_debug("cpufreq: cpu%d init at %d switching to %d\n",
 			policy->cpu, cur_freq, table[index].frequency);
-
 	policy->cur = table[index].frequency;
 	cpufreq_frequency_table_get_attr(table, policy->cpu);
 
@@ -282,64 +274,6 @@ static int msm_cpufreq_resume(void)
 	return NOTIFY_DONE;
 }
 
-/** max freq interface **/
-
-static ssize_t show_ex_max_freq(struct cpufreq_policy *policy, char *buf)
-{
-	if (!ex_max_freq)
-		ex_max_freq = policy->max;
-
-	return sprintf(buf, "%u\n", ex_max_freq);
-}
-
-static ssize_t store_ex_max_freq(struct cpufreq_policy *policy,
-		const char *buf, size_t count)
-{
-	unsigned int freq = 0;
-	int ret, cpu;
-	int index;
-	struct cpufreq_frequency_table *freq_table = cpufreq_frequency_get_table(policy->cpu);
-
-	if (!freq_table)
-		return -EINVAL;
-
-	ret = sscanf(buf, "%u", &freq);
-	if (ret != 1)
-		return -EINVAL;
-
-	mutex_lock(&per_cpu(cpufreq_suspend, policy->cpu).suspend_mutex);
-
-	ret = cpufreq_frequency_table_target(policy, freq_table, freq,
-			CPUFREQ_RELATION_H, &index);
-	if (ret)
-		goto out;
-
-	ex_max_freq = freq_table[index].frequency;
-
-	for_each_possible_cpu(cpu) {
-		msm_cpufreq_set_freq_limits(cpu, MSM_CPUFREQ_NO_LIMIT, ex_max_freq);
-	}
-	cpufreq_update_policy(cpu);
-
-	ret = count;
-
-out:
-	mutex_unlock(&per_cpu(cpufreq_suspend, policy->cpu).suspend_mutex);
-	return ret;
-}
-
-struct freq_attr msm_cpufreq_attr_ex_max_freq = {
-	.attr = { .name = "ex_max_freq",
-		.mode = 0666,
-	},
-	.show = show_ex_max_freq,
-	.store = store_ex_max_freq,
-};
-/** end max freq interface **/
-
-
-/** end maxscreen off sysfs interface **/
-
 static int msm_cpufreq_pm_event(struct notifier_block *this,
 				unsigned long event, void *ptr)
 {
@@ -361,9 +295,6 @@ static struct notifier_block msm_cpufreq_pm_notifier = {
 
 static struct freq_attr *msm_freq_attr[] = {
 	&cpufreq_freq_attr_scaling_available_freqs,
-	&msm_cpufreq_attr_max_screen_off_khz,
-	&msm_cpufreq_attr_max_screen_off,
-	&msm_cpufreq_attr_ex_max_freq,
 	NULL,
 };
 
