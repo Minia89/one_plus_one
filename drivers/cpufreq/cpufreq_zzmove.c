fa86c998a8c7a139b08a1eee926194eea064431d
Minia89 <n.miniati@gmail.com>
2015-03-25 21:20:33 +0100

    zzmove: changes

--- drivers/cpufreq/cpufreq_zzmove.c
+++ drivers/cpufreq/cpufreq_zzmove.c
@@ -6838,7 +6838,7 @@ static inline void dbs_timer_init(struct cpu_dbs_info_s *dbs_info)
 
 	dbs_info_enabled = true;
 
-	INIT_DEFERRABLE_WORK(&dbs_info->work, do_dbs_timer);
+	INIT_DELAYED_WORK_DEFERRABLE(&dbs_info->work, do_dbs_timer);
 
 	queue_delayed_work_on(dbs_info->cpu, dbs_wq, &dbs_info->work, delay);
 #ifdef ENABLE_WORK_RESTARTLOOP
